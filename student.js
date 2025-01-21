9db1d5f3deded84ebdc5a2b617e5f9004a05e374
sup <sup>
2025-01-21 22:15:41 +0530

    final

--- student.js
+++ student.js
@@ -1,41 +1,101 @@
-import { DynamoDBClient } from "/node_modules/@aws-sdk/client-dynamodb/dist-es/DynamoDBClient.js";
-import { DynamoDBDocumentClient } from "/node_modules/@aws-sdk/lib-dynamodb/dist-es/DynamoDBDocumentClient.js";
-import { GetCommand } from "/node_modules/@aws-sdk/lib-dynamodb/dist-es/commands/GetCommand.js";
+import { DynamoDBClient } from "./node_modules/@aws-sdk/client-dynamodb/dist-es/DynamoDBClient.js";
+import { DynamoDBDocumentClient } from "./node_modules/@aws-sdk/lib-dynamodb/dist-es/DynamoDBDocumentClient.js";
+import { GetCommand } from "./node_modules/@aws-sdk/lib-dynamodb/dist-es/commands/GetCommand.js";
+import { ScanCommand } from "./node_modules/@aws-sdk/client-dynamodb/dist-es/commands/ScanCommand.js";
 
-const client = new DynamoDBClient({});
+import 'url';
+
+var currURL = new URL(window.location.href);
+
+const cred = {
+    accessKeyId: import.meta.env.VITE_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.VITE_SECRET_ACCESS_KEY
+};
+
+const client = new DynamoDBClient({
+	region : "ap-south-1",
+	credentials : cred
+});
 const docClient = DynamoDBDocumentClient.from(client);
 
-table = document.getElementById("studentTable");
+export const main = async () => {
+	var table = document.getElementById("studentTable");
+	var percentageTable = document.getElementById("attendancePercentage");
 
-let classNum;
-let studentData= docClient.send(new GetCommand({TableName:"students",
-							  Key:{id:"A001"},
-							 }));
-let totalClasses = studentData["Item"]["classes_attended"];
 
-for (classNum = 1; classNum <= totalClasses; classNum++)
-{
-	command = new GetCommand({
-				 TableName:"attendance",
-				 Key:{
-					student_id:"A001",
-					student_class_number: classNum
-				     }
-				 });
+	var teacherIds = await docClient.send(await new ScanCommand({
+																	TableName:"teachers",
+																	AttributesToGet: ["id"]
+																}));
 
-	response = docClient.send(command);
-	console.log(response["Item"]);
+	let studentId = currURL.searchParams.get("id");
+	var studentData = await docClient.send(await new GetCommand({
+																	TableName:"students",
+																	Key:{id:studentId},
+																}));
+	document.getElementById("greeting").innerHTML = "Hello, " + studentData["Item"]["name"] + "!";
 
-	let newRow = table.insertRow(classNum - 1);
-	let studentNameCell = newRow.insertCell(0);
-	let teacherNameCell = newRow.insertCell(1);
-	let dateCell = newRow.insertCell(2);
-	let classStartTimeCell = newRow.insertCell(3);
-	let attendingTimeCell = newRow.insertCell(4);
+	for (let i = 0; i < teacherIds["Items"].length; i++)
+	{
+		let classNum;
 
-	studentNameCell.innerHTML = response["Item"]["student_id"];
-	teacherNameCell.innerHTML = response["Item"]["teacher_id"];
-	dateCell.innerHTML = response["Item"]["date"];
-	classStartTimeCell.innerHTML = response["Item"]["class_start_time"];
-	attendingTimeCell.innerHTML = response["Item"]["attending_time"];
+		let teacherData = await docClient.send(await new GetCommand({
+														TableName:"teachers",
+														Key:{id:teacherIds["Items"][i]["id"]["S"]}
+														}));
+    	let attendedClasses = 0;
+		let totalClasses = teacherData["Item"]["classes_held"];
+		let attendancePercentage;
+	
+		let row = percentageTable.insertRow();
+		let teacherName = row.insertCell(0);
+		let percent = row.insertCell(1);
+
+    	row.style.textAlign = "center"
+		teacherName.style.textAlign = "center";
+		percent.style.textAlign = "center";
+		teacherName.innerHTML = teacherData["Item"]["name"];
+		
+
+		for (classNum = 1; classNum <= totalClasses; classNum++)
+		{
+			var command = await new GetCommand({
+						 							TableName:"classes",
+						 							Key:{
+															teacher_id:teacherIds["Items"][i]["id"]["S"],
+															class_number: classNum
+					    		 						}
+					 							});
+
+			var response = await docClient.send(command);
+		
+			let attendingStudents = response["Item"]["attending_students"];
+			let attendingStatus = (attendingStudents.has(studentId)) ? "Present" : "Absent";
+
+			let newRow = table.insertRow();
+			let teacherNameCell = newRow.insertCell(0);
+			let dateCell = newRow.insertCell(1);
+			let classStartTimeCell = newRow.insertCell(2);
+			let classEndTimeCell = newRow.insertCell(3);
+			let status = newRow.insertCell(4);
+
+			teacherNameCell.innerHTML = teacherData["Item"]["name"];
+			dateCell.innerHTML = response["Item"]["date"];
+			classStartTimeCell.innerHTML = response["Item"]["class_start_time"];
+			classEndTimeCell.innerHTML = response["Item"]["class_end_time"];
+			status.innerHTML = attendingStatus;
+			if (attendingStatus == "Absent")
+				status.style.color = "red";
+			else
+			{
+				attendedClasses += 1;
+				status.style.color = "green";							
+			}
+		}	
+		
+		attendancePercentage = (attendedClasses / totalClasses) * 100;
+		percent.innerHTML = attendancePercentage.toFixed(2) + "%";
+	}
 }
+
+main();
